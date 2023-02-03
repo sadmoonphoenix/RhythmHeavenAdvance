bb9787e37cb1923f36d34a872cf57d9c35fcff9f
Shaffy <thedave2cz@gmail.com>
2023-02-03 18:35:18 +0100

    perl no longer needed
     (not really but shitty workaround)

--- compile.bat
+++ compile.bat
@@ -1,5 +1,12 @@
 @echo off
 
+set perl-path="perl"
+
+if exist "tools/perl.exe" (
+	echo Found perl override, using that for compilation.
+	set perl-path="tools/perl.exe"
+)
+
 :check
 if exist "rh-jpn.gba" (
 	goto tools
@@ -10,7 +17,7 @@ if exist "rh-jpn.gba" (
 :tools
 mkdir build
 copy "rh-jpn.gba" "build\rh-atlus.gba"
-perl "tools\abcde\abcde.pl" -cm abcde::Atlas "build\rh-atlus.gba" "src\script.txt"
+%perl-path% "tools\abcde\abcde.pl" -cm abcde::Atlas "build\rh-atlus.gba" "src\script.txt"
 
 tools\4bmpp.exe -p gfx/RhythmGames/NightWalk/beba58_night1_intro.bmp
 tools\4bmpp.exe -p gfx/RhythmGames/KarateMan/BE0480_karateman_tiles.bmp
--- fast_compile.bat
+++ fast_compile.bat
@@ -1,5 +1,12 @@
 @echo off
 
+set perl-path="perl"
+
+if exist "tools/perl.exe" (
+	echo Found perl override, using that for compilation.
+	set perl-path="tools/perl.exe"
+)
+
 :check
 if exist "rh-jpn.gba" (
 	goto tools
@@ -10,7 +17,7 @@ if exist "rh-jpn.gba" (
 :tools
 mkdir build
 copy "rh-jpn.gba" "build\rh-atlus.gba"
-perl "tools\abcde\abcde.pl" -cm abcde::Atlas "build\rh-atlus.gba" "src\script.txt"
+%perl-path% "tools\abcde\abcde.pl" -cm abcde::Atlas "build\rh-atlus.gba" "src\script.txt"
 
 tools\armips.exe src/main.asm
 echo Done, this script only and ONLY assemble the graphics and text into the ROM, if the graphics were updated, then it will not update them, you should compile them again. (Press any key to recompile!)
