57e880f53cf49cddb9bfeee91e170068c4205e16
Shaffy <thedave2cz@gmail.com>
2023-04-08 10:17:30 +0200

    options

--- gfx/Options/options.asm
+++ gfx/Options/options.asm
@@ -3,3 +3,51 @@
 
 .org 0x088F57C8
 .import "gfx/Options/8F57C8_assembly0.bin"
+
+
+.org 0x088F595C
+.import "gfx/Options/8F595C_assembly0.bin"
+
+.org 0x088F5988
+.import "gfx/Options/8F5988_assembly1.bin"
+
+.org 0x088F59B4
+.import "gfx/Options/8F59B4_assembly2.bin"
+
+.org 0x088F59E0
+.import "gfx/Options/8F59E0_assembly3.bin"
+
+.org 0x088F5A0C
+.import "gfx/Options/8F5A0C_assembly4.bin"
+
+.org 0x088F584E
+.import "gfx/Options/8F584E_assembly0.bin"
+
+
+.org 0x088F5880
+.import "gfx/Options/8F5880_assembly1.bin"
+
+.org 0x088F58B2
+.import "gfx/Options/8F58B2_assembly2.bin"
+
+.org 0x088F58E4
+.import "gfx/Options/8F58E4_assembly3.bin"
+
+.org 0x088F5916
+.import "gfx/Options/8F5916_assembly4.bin"
+
+
+.org 0x088F5A38
+.import "gfx/Options/8F5A38_assembly0.bin"
+
+
+.org 0x088F5A46
+.import "gfx/Options/8F5A46_assembly0.bin"
+
+
+.org 0x088F5A78
+.import "gfx/Options/8F5A78_assembly0.bin"
+
+
+.org 0x088F5948
+.import "gfx/Options/8F5948_assembly0.bin"
