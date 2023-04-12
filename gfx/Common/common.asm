0b46769b7bfe80f87471f70c39260bd4c8b77afb
Shaffy <thedave2cz@gmail.com>
2023-04-12 21:40:55 +0200

    Pause menu

--- gfx/Common/common.asm
+++ gfx/Common/common.asm
@@ -2,3 +2,27 @@
 .dw CCCA5C_common_tiles_dat
 .dw CCCA5C_common_tiles_rle
 .dw CCCA5C_common_tiles_rle_end - CCCA5C_common_tiles_rle
+
+.org 0x0890AB88
+.dw X90A882_assembly0
+
+.org 0x0890AB90
+.dw X90A89C_assembly1
+
+.org 0x0890AB98
+.dw X90A882_assembly2
+
+.org 0x0890ABA0
+.dw X90A8B6_assembly3
+
+.org 0x0890ABB0
+.dw X90A8D0_assembly0
+
+.org 0x0890ABB8
+.dw X90A8EA_assembly1
+
+.org 0x0890ABC0
+.dw X90A8D0_assembly2
+
+.org 0x0890ABC8
+.dw X90A904_assembly3
--- gfx/Common/common_relocate.asm
+++ gfx/Common/common_relocate.asm
@@ -5,3 +5,35 @@ CCCA5C_common_tiles_dat:
 CCCA5C_common_tiles_rle:
 .import "gfx/Common/CCCA5C_common_tiles.rle"
 CCCA5C_common_tiles_rle_end:
+
+.align 2
+X90A882_assembly0:
+.import "gfx/Common/90A882_assembly0.bin"
+
+.align 2
+X90A89C_assembly1:
+.import "gfx/Common/90A89C_assembly1.bin"
+
+.align 2
+X90A882_assembly2:
+.import "gfx/Common/90A882_assembly2.bin"
+
+.align 2
+X90A8B6_assembly3:
+.import "gfx/Common/90A8B6_assembly3.bin"
+
+.align 2
+X90A8D0_assembly0:
+.import "gfx/Common/90A8D0_assembly0.bin"
+
+.align 2
+X90A8EA_assembly1:
+.import "gfx/Common/90A8EA_assembly1.bin"
+
+.align 2
+X90A8D0_assembly2:
+.import "gfx/Common/90A8D0_assembly2.bin"
+
+.align 2
+X90A904_assembly3:
+.import "gfx/Common/90A904_assembly3.bin"
