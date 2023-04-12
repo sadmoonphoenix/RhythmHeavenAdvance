57ebe76309eaa284f090d471897f3af480a59f1d
Shaffy <thedave2cz@gmail.com>
2023-04-12 14:34:07 +0200

    fix

---
+++ gfx/RhythmToys/ConfessionMachine/confessionmachine_relocate.asm
@@ -0,0 +1,12 @@
+.align 4
+CDD0D4_confessionmachine_bg:
+.import "gfx/RhythmToys/ConfessionMachine/CDD0D4_confessionmachine_bg.cdat"
+
+.align 4
+CDD0D4_confessionmachine_map_dat:
+.import "gfx/RhythmToys/ConfessionMachine/CDD0D4_confessionmachine_map.dat"
+
+.align 4
+CDD0D4_confessionmachine_map_rle:
+.import "gfx/RhythmToys/ConfessionMachine/CDD0D4_confessionmachine_map.rle"
+CDD0D4_confessionmachine_map_rle_end:
