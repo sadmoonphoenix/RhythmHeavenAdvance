0b9596af6b8f7896a6077e283a9e1d3fcd1413fe
Moon <64157802+sadmoonphoenix@users.noreply.github.com>
2023-11-18 01:40:19 -0800

    push

--- compile.ps1
+++ compile.ps1
@@ -2,82 +2,85 @@
 
 # clean this out
 
-$script:perl-path = "perl"
+$script:perl_path="perl"
 
-if exist "tools\win\perl.exe" (
-	echo Found perl override, using that for compilation.
-	set perl-path="tools\win\perl.exe"
-)
+if (Test-Path -Path "tools\win\perl.exe" -PathType Leaf) {
+	Write-Output "Found perl override, using that for compilation."
+	$script:perl_path="tools\win\perl.exe"
+}
 
 
 
-:check
-if exist "rh-jpn.gba" (
-	goto tools
-) else (
-	goto nofile
-)
+function Confirm-ROMExistence {
+  if (Test-Path -Path "rh-jpn.gba" -PathType Leaf) {
+    Start-ROMCompilation
+  } else {
+    Show-NoFileError
+  }
+}
 
-:tools
-mkdir build
-copy "rh-jpn.gba" "build\rh-atlus.gba"
+function Start-ROMCompilation {
+  New-Item -Path . -Name "build" -ItemType "directory"
+  Copy-Item "rh-jpn.gba" -Destination "build"
 
-echo -- Compile Text --
+  Write-Output "-- Compile Text --"
 
-%perl-path% "tools\abcde\abcde.pl" -cm abcde::Atlas "build\rh-atlus.gba" "src\script.txt"
+  $perl_path "tools\abcde\abcde.pl" -cm abcde::Atlas "build\rh-atlus.gba" "src\script.txt"
 
-echo -- Compile Bitmap --
+  echo -- Compile Bitmap --
 
-setlocal EnableDelayedExpansion
-for /f "skip=1 delims=" %%f in (src/bitmaps_to_compile.md) do (
-  set "file=%%f"
-  tools\win\4bmpp.exe -p !file!
-)
-endlocal
+  setlocal EnableDelayedExpansion
+  for /f "skip=1 delims=" %%f in (src/bitmaps_to_compile.md) do (
+    set "file=%%f"
+    tools\win\4bmpp.exe -p !file!
+  )
+  endlocal
 
-echo -- Compile Graphics --
+  echo -- Compile Graphics --
 
-setlocal EnableDelayedExpansion
-for /f "skip=1 delims=" %%f in (src/graphics_to_compile.md) do (
-  set "file=%%f"
-  tools\win\DSDecmp.exe -c lz10 !file!.bin !file!
-)
-endlocal
+  setlocal EnableDelayedExpansion
+  for /f "skip=1 delims=" %%f in (src/graphics_to_compile.md) do (
+    set "file=%%f"
+    tools\win\DSDecmp.exe -c lz10 !file!.bin !file!
+  )
+  endlocal
 
-echo -- Compile Tile Maps ---
+  echo -- Compile Tile Maps ---
 
-setlocal EnableDelayedExpansion
-for /f "skip=1 delims=" %%f in (src/tilemaps_to_compile.md) do (
-  set "file=%%f"
-  tools\win\rhcomp.exe !file!
-)
-endlocal
+  setlocal EnableDelayedExpansion
+  for /f "skip=1 delims=" %%f in (src/tilemaps_to_compile.md) do (
+    set "file=%%f"
+    tools\win\rhcomp.exe !file!
+  )
+  endlocal
 
-echo -- Compile Audio --
+  echo -- Compile Audio --
 
-setlocal EnableDelayedExpansion
-for /f "skip=1 delims=" %%f in (src/sounds_to_compile.md) do (
-  set "file=%%f"
-  echo !file!
-  ffmpeg -y -i !file!.wav -acodec pcm_s8 -ar 13379 -ac 1 -f s8 !file!.pcm -loglevel error
-)
-endlocal
+  setlocal EnableDelayedExpansion
+  for /f "skip=1 delims=" %%f in (src/sounds_to_compile.md) do (
+    set "file=%%f"
+    echo !file!
+    ffmpeg -y -i !file!.wav -acodec pcm_s8 -ar 13379 -ac 1 -f s8 !file!.pcm -loglevel error
+  )
+  endlocal
 
-echo -- Compile Code --
+  echo -- Compile Code --
 
-tools\win\armips.exe src/main.asm
-if %ERRORLEVEL% NEQ 0 (
-    goto fail
-)
+  tools\win\armips.exe src/main.asm
+  if %ERRORLEVEL% NEQ 0 (
+      goto fail
+  )
 
-color 0f
-echo Building complete (Press any key to recompile!)
-del "build\rh-atlus.gba"
+  color 0f
+  echo Building complete (Press any key to recompile!)
+  del "build\rh-atlus.gba"
 
-pause > nul
+  pause > nul
 
-cls
-goto check
+  cls
+  goto check
+
+}
 
 :nofile
 echo Couldn't find a Rhythm Tengoku ROM, please place one in the root of the project to continue.
